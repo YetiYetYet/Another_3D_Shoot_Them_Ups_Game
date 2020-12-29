c48afe68205ac44123c225888d85c280e5ccdf27
YetiYetYet <alexisbehier22@gmail.com>
2020-12-29 23:36:46 +0100

    First Init, simple controller, animator and animation of player and enemy

---
+++ Assets/Scripts/SimpleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleController : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, speed, 0);
+            animator.SetBool("IsMoving", true);
+            animator.SetBool("MovingToLeft", true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, -speed, 0);
+            animator.SetBool("IsMoving", true);
+            animator.SetBool("MovingToLeft", false);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
+    }
+}
