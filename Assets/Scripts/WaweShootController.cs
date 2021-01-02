1ca67f4dd954da16f37cbfdddca8c4feec1caaab
YetiYetYet <alexisbehier22@gmail.com>
2021-01-02 01:12:13 +0100

    Adding wawe bullet management

---
+++ Assets/Scripts/WaweShootController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaweShootController : MonoBehaviour
+{
+    public int stateID;
+    public List<UbhBaseShot> UbhBaseShotsChild;
+    public int actualShotCtrlIndex;
+    private void Start()
+    {
+        UbhBaseShotsChild = new List<UbhBaseShot>();
+        Debug.Log(gameObject.GetComponentsInChildren<UbhBaseShot>().Length);
+        foreach (var ubhShotCtrl in gameObject.GetComponentsInChildren<UbhBaseShot>())
+        {
+            UbhBaseShotsChild.Add(ubhShotCtrl);
+        }
+
+        if (UbhBaseShotsChild.Count == 0)
+        {
+            Debug.LogError("No Pattern Found in child");
+        }
+
+        actualShotCtrlIndex = 0;
+
+        foreach (var ubhBaseShot in UbhBaseShotsChild)
+        {
+            ubhBaseShot.m_shotFinishedCallbackEvents.AddListener(NextPattern);
+        }
+
+        for (int i = 1; i < UbhBaseShotsChild.Count; i++)
+        {
+            UbhBaseShotsChild[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void NextPattern()
+    {
+        Debug.Log("NextPatternCall");
+        UbhBaseShotsChild[actualShotCtrlIndex].gameObject.SetActive(false);
+        actualShotCtrlIndex++;
+        if (actualShotCtrlIndex > UbhBaseShotsChild.Count-1)
+        {
+            actualShotCtrlIndex = 0;
+        }
+        UbhBaseShotsChild[actualShotCtrlIndex].gameObject.SetActive(true);
+    }
+}
